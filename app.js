1fe967f7f582719b9e8b02656769a6907afaf2b2
NischalShrestha <nischalshrestha324@gmain.com>
2023-07-03 09:44:36 +0545

    first ejs

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+
+    var today = new Date();
+    var currentDay = today.getDate();
+    var day = "";
+
+    switch (currentDay) {
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wenesday";
+            break;
+        case 4:
+            day = "Tuesday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        default:
+            console.log("Current day is equal to" + currentDay);
+            break;
+        
+    }
+
+    res.render("list", { kindOfDay: day});
+
 });
 
 app.listen(3000, (req, res) => {
